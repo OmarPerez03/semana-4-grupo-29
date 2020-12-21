4bc1605ec10f56a2c195f41b28b3036926b95ebc
OmarPerez03 <75846101+OmarPerez03@users.noreply.github.com>
2020-12-20 19:51:01 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+
+    <image-text></image-text>
+    <carousel-section></carousel-section>
+    <services-section></services-section>
+
+    <!--  news -->
+    <div id="news" class="container-fluid">
+      <news-section></news-section>
+    </div>
+    <!--  news -->
+
+    <!--  members -->
+    <div class="container-fluid">
+      <div class="row justify-content-center mb-5">
+        <div class="col mt-5" v-for="(miembro, index) of Team" :key="index" >
+          <team-card :member="miembro"></team-card>
+        </div>  
+      </div>
+    </div>
+    <!--  members -->
+
+    <final-section></final-section>
+
+  </div>
+</template>
+
+
+<script>
+
+import ImageText from './components/ImageText.vue';
+import CarouselSection from './components/CarouselSection.vue';
+import ServicesSection from './components/ServicesSection.vue';
+import FinalSection from './components/FinalSection.vue';
+import TeamCard from './components/TeamCard.vue';
+import NewsSection from "./components/NewsSection.vue"
+
+export default {
+  name: 'App',
+  components:{
+    ImageText,
+    CarouselSection,
+    ServicesSection,
+    FinalSection,
+    TeamCard,
+    NewsSection
+  },
+  data(){
+    return{
+      Team: [
+      {
+        codigo : 1 ,
+        nombre : 'Luisa María' ,
+        descripcion : 'Egresada del programa de Matemáticas en la Universidad Nacional de Colombia sede Medellín. Intereses en ciencia de datos y machine learning.' ,
+        rol : 'Desarrolladora' ,
+        image : 'https://i.ibb.co/gvpPsqx/foto-Luisa4.png'
+      },
+      {
+        codigo : 2 ,
+        nombre : 'Omar Andrés' ,
+        descripcion : 'Estudiante ingenieria de sistemas en la Universidad Central con diplomado en programacion java en la Universidad Politecnica de Colombia.' ,
+        rol : 'Desarrollador' ,
+        image : 'https://i.ibb.co/QFqrG46/omar-Circular.png'
+      },
+      {
+        codigo : 3 ,
+        nombre : 'Martha Cecilia' ,
+        descripcion : 'Egresada de la universidad Real de Colombia del técnico en programación, actualmente estudia en el SENA mejorando su currículo profesional.' ,
+        rol : 'Desarrolladora' ,
+        image : 'https://i.ibb.co/k1WXhww/martha-Circular.png'
+      }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/FinalSection.vue
@@ -0,0 +1,55 @@
+<template>
+    <!-- Footer -->
+    <footer id="footer" class="page-footer bg-secondary text-white pt-4">
+        <!-- Footer Text -->
+        <div class="container-fluid text-center text-md-left">
+            <!-- Grid row -->
+            <div class="row">
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Sobre nosotros</h5>
+                    <a href="https://github.com/lumrodriguezca/semana-2-26"> Conoce nuestro repositorio </a>
+                </div>
+                <!-- Grid column -->
+                <hr class="clearfix w-100 d-md-none pb-3">
+                <!-- Grid column -->
+                <div class="col-md-6 mb-md-0 mb-3">
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Contacto</h5>
+                    <dl class="row">
+                        
+                        <dt class="col-sm-3"><a class="linkPerfil" href="https://github.com/lumrodriguezca" target="_blank">Luisa María</a></dt>
+                        <dd class="col-sm-9">lumrodriguezca@unal.edu.co</dd>
+                        <dt class="col-sm-3"><a class="linkPerfil" href="https://github.com/OmarPerez03" target="_blank">Omar Andrés</a></dt>
+                        <dd class="col-sm-9">omar.andres.03.06@gmail.com</dd>
+                        <dt class="col-sm-3"><a class="linkPerfil" href="https://github.com/Mao182532" target="_blank">Martha Cecilia</a></dt>
+                        <dd class="col-sm-9">marthaceciliaortizmendez@gmail.com</dd>
+                    </dl>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+        <!-- Footer Text -->
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-1 bg-dark">© 2020 Copyright: DAD
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+</template>
+
+
+<script>
+    export default {
+        name: "FinalSection",
+    }
+</script>
+
+
+<style scoped>
+.linkPerfil{
+    color: white;
+  }
+</style>
